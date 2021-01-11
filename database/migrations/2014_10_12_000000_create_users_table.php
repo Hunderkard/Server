2232847262dc4036c44c8d3ef1be2bd030cae4e6
Hunderkard <juanmanuelmartel1996@gmail.com>
2021-01-11 23:36:24 +0100

    Migracion y Semillacion Usuario

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('level');    //ps Esta se la pongo yo. De 0->admin a 4->usuario.
             $table->rememberToken();
             $table->timestamps();
         });
